d235b508e09383b36efeedcd8fd5577b4d21af66
Eiji MATSUMOTO <e.mattsan@gmail.com>
2016-05-03 23:33:32 +0900

    remove tree node array

--- lib/myers_diff.rb
+++ lib/myers_diff.rb
@@ -1,7 +1,7 @@
 require "myers_diff/version"
 
 module MyersDiff
-  VItem = Struct.new(:y, :tail)
+  VItem = Struct.new(:y, :node)
   TreeNode = Struct.new(:edit_type, :prev)
 
   def self.diff(sequence1, sequence2)
@@ -9,7 +9,6 @@ module MyersDiff
     size2 = sequence2.size
 
     v = (size1 + size2 + 1).times.map { VItem.new(0, nil) }
-    tree = []
 
     (size1 + size2 + 1).times do |d|
       (-d..d).step(2) do |k|
@@ -22,28 +21,24 @@ module MyersDiff
         if d != 0
           if (k == -d) || (k == -size1) || ((k != d) && (k != size2) && ((v_km1.y + 1) < v_kp1.y))
             v_k.y = v_kp1.y
-            v_k.tail = tree.size
-            tree.push TreeNode.new(:DELETE, v_kp1.tail)
+            v_k.node = TreeNode.new(:DELETE, v_kp1.node)
           else
             v_k.y = v_km1.y + 1
-            v_k.tail = tree.size
-            tree.push TreeNode.new(:ADD, v_km1.tail)
+            v_k.node = TreeNode.new(:ADD, v_km1.node)
           end
         end
 
         while ((v_k.y - k) < size1) && (v_k.y < size2) && (sequence1[v_k.y - k] == sequence2[v_k.y])
-          node = TreeNode.new(:COMMON, v_k.tail)
-          v_k.tail = tree.size
-          tree.push node
+          v_k.node = TreeNode.new(:COMMON, v_k.node)
           v_k.y += 1
         end
 
         if ((v_k.y - k) >= size1) && (v_k.y >= size2)
           ses = []
-          i = v_k.tail
-          while i
-            ses.push tree[i].edit_type
-            i = tree[i].prev
+          node = v_k.node
+          while node
+            ses.push node.edit_type
+            node = node.prev
           end
           return ses.reverse
         end
